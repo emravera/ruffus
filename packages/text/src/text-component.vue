d6760acfefa1424458a9a8eae5f4eb08f44de99b
emravera <ravera.emanuel@gmail.com>
2022-03-21 18:14:26 -0300

    First test of lerna release

---
+++ packages/text/src/text-component.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1 class="size-1">
+        {{ text }}
+    </h1>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            default: '',
+            required: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .size-1 {
+        font-size: 48px;
+    }
+</style>
