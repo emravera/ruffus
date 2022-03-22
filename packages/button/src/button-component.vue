1beb2b9d2785b3089ff00a296fa724f682c8b973
emravera <ravera.emanuel@gmail.com>
2022-03-22 16:00:00 -0300

    Added button as new component

---
+++ packages/button/src/button-component.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="color">
+        <text-component> {{ text }} </text-component>
+    </button>
+</template>
+
+<script>
+import TextComponent from '@ruffus/text';
+export default {
+    props: {
+        text: {
+            type: String,
+            default: '',
+            required: false
+        }
+    },
+    components: {
+        TextComponent
+    }
+}
+</script>
+
+<style scoped>
+    .color {
+        color: blue;
+    }
+</style>
